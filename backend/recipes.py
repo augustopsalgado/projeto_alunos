66cd8ea76929c4fac30ce098616cb6b5e6abc3ec
augustopsalgado <42944134+augustopsalgado@users.noreply.github.com>
2022-12-23 16:16:13 -0300

    update

--- backend/recipes.py
+++ backend/recipes.py
@@ -1,10 +1,12 @@
-from flask_restx import Namespace, Resource, fields 
+from flask_restx import Namespace,Resource,fields
 from models import Recipe
 from flask_jwt_extended import jwt_required
+from flask import request
 
-recipe_ns=Namespace('recipe',description="O namespace para as recipes")
 
-#modelo do serialiazer
+recipe_ns=Namespace('recipe',description="A namespace for Recipes")
+
+
 recipe_model=recipe_ns.model(
     "Recipe",
     {
@@ -13,27 +15,35 @@ recipe_model=recipe_ns.model(
         "description":fields.String()
     }
 )
+
+
+
+
 @recipe_ns.route('/hello')
 class HelloResource(Resource):
     def get(self):
         return {"message":"Hello World"}
+
+
 
 @recipe_ns.route('/recipes')
 class RecipesResource(Resource):
 
     @recipe_ns.marshal_list_with(recipe_model)
     def get(self):
-        """ busca todas receitas"""
-        
+        """Get all recipes """
+
         recipes=Recipe.query.all()
 
+
         return recipes
+
 
     @recipe_ns.marshal_with(recipe_model)
     @recipe_ns.expect(recipe_model)
     @jwt_required()
     def post(self):
-        """cria uma nova receita"""
+        """Create a new recipe"""
 
         data=request.get_json()
 
@@ -47,21 +57,25 @@ class RecipesResource(Resource):
         return new_recipe,201
 
 
+
+
 @recipe_ns.route('/recipe/<int:id>')
 class RecipeResource(Resource):
 
     @recipe_ns.marshal_with(recipe_model)
     def get(self,id):
-        """ busca pelo id"""
+        """Get a recipe by id """
         recipe=Recipe.query.get_or_404(id)
 
         return recipe
 
+
     @recipe_ns.marshal_with(recipe_model)
     @jwt_required()
     def put(self,id):
-        """"atualiza por id"""
-        
+        """Update a recipe by id """
+
+
         recipe_to_update=Recipe.query.get_or_404(id)
 
         data=request.get_json()
@@ -70,12 +84,14 @@ class RecipeResource(Resource):
 
         return recipe_to_update
 
+
     @recipe_ns.marshal_with(recipe_model)
     @jwt_required()
     def delete(self,id):
-        """"Deleta por id"""
+        """Delete a recipe by id """
+
         recipe_to_delete=Recipe.query.get_or_404(id)
 
         recipe_to_delete.delete()
 
-        return recipe_to_delete  
+        return recipe_to_delete
